c7a77cd76854dc2225979820c80bc85d349f57cd
darshanshah1996 <darshan@lawlytics.com>
2023-07-03 11:01:13 +0530

    Demo project for implementing dynamic configuration in CircleCI

---
+++ test/specs/prod_regression.js
@@ -0,0 +1,10 @@
+import { expect as chaiExpect } from "chai";
+describe("Facebook Homepage", () => {
+  it("should have correct title", async () => {
+    await browser.url("https://www.facebook.com/");
+
+    const pageTitle = await browser.getTitle();
+
+    chaiExpect(pageTitle).to.equal("Facebook – log in or sign up");
+  });
+});
---
+++ test/specs/staging_regression.js
@@ -0,0 +1,10 @@
+import { expect as chaiExpect } from "chai";
+describe("Youtube Homepage", () => {
+  it("should have correct title", async () => {
+    await browser.url("https://www.youtube.com/");
+
+    const pageTitle = await browser.getTitle();
+
+    chaiExpect(pageTitle).to.equal("YouTube");
+  });
+});
