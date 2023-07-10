5d44babe3fb03ccd7488d3af21d6e0a34e042fb7
darshanshah1996 <ds62730@gmail.com>
2023-07-10 11:38:49 +0530

    Update config file (#2)
    * Update config file
    
    * Update Readme
    
    * Update readme

--- specs.js
+++ specs.js
@@ -1,5 +1,13 @@
 export const sanity = ["./test/specs/google_homepage_test.js"];
 
-export const stagingRegression = ["./test/specs/youtube_homepage_test.js"];
+export const stagingRegression = [
+  "./test/specs/youtube_homepage_test.js",
+  "./test/specs/google_homepage_test.js",
+  "./test/specs/facebook_homepage_test.js",
+];
 
-export const prodRegression =["./test/specs/facebook_homepage_test.js"]
+export const prodRegression = [
+  "./test/specs/youtube_homepage_test.js",
+  "./test/specs/google_homepage_test.js",
+  "./test/specs/facebook_homepage_test.js",
+];
